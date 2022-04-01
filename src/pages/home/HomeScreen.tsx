c0598eb2247c71c731dfe6612092d79942eab422
Ivan Lara <ukikalix@gmail.com>
2022-04-01 07:58:10 -0600

    Initial commit

---
+++ src/pages/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeScreen = () => {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            Welcome to my porftolio
+            <span>by Ivan Lara</span>
+        </div>
+    );
+};
+
+export default HomeScreen;
