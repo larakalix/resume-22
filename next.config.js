231dcf62c9d1b01560b1dc992c3002a578a7d842
Ivan Lara <ukikalix@gmail.com>
2022-04-01 08:08:38 -0600

    Add dato cms lib client

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    env: {
+        NEXT_DATO_CMS_TOKEN: process.env.NEXT_DATO_CMS_TOKEN,
+        NEXT_TITLE: process.env.NEXT_TITLE,
+    },
+    images: {
+        domains: ["www.datocms-assets.com"],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
