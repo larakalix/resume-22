8439289fcf911fc0aa6d68845c32c72cd6f49d67
Ivan Lara <ukikalix@gmail.com>
2022-04-01 08:22:24 -0600

    Add tailwindcss config

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,11 @@ import type { NextPage } from "next";
 import HomeScreen from "../src/pages/home/HomeScreen";
 
 const Home: NextPage = () => {
-    return <HomeScreen />;
+    return (
+        <div className="bg-gray-100">
+            <HomeScreen />
+        </div>
+    );
 };
 
 export default Home;
