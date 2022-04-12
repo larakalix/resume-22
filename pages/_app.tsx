98896930ca97e13e8c5d7dd7389ebedd33c74460
Ivan Lara <uki@live.co.uk>
2022-04-12 16:53:28 -0600

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,6 +19,22 @@ function MyApp({ Component, pageProps }: AppProps) {
             <div className={`${theme}`}>
                 <Head>
                     <link rel="shortcut icon" href="/favicon.png" />
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
                 </Head>
                 <div className="bg-gray-100 dark:bg-black scroll-smooth transition-colors">
                     <Header />
