a973f2a55fba0de94a3d87196a68e1499e7f6faf
Ivan Lara <uki@live.co.uk>
2022-04-11 23:56:39 -0600

    Add head to pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import ContactWrap from "../src/components/generic/ContactWrap";
 import { AboutScreen } from "../src/pages/about/AboutScreen";
 
 const About: NextPage = () => {
     return (
         <ContactWrap>
+            <Head>
+                <title>About me</title>
+            </Head>
             <AboutScreen />
         </ContactWrap>
     );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import ContactWrap from "../src/components/generic/ContactWrap";
 import { HomeScreen } from "../src/pages";
 
 const Home: NextPage = () => {
     return (
         <ContactWrap>
+            <Head>
+                <title>Hello stranger!</title>
+            </Head>
             <HomeScreen />
         </ContactWrap>
     );
