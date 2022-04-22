1dbf9bc9aed0db94c1b97492e45d226d86981b0f
Ivan Lara <uki@live.co.uk>
2022-04-22 10:38:17 -0600

    Fix project buttons

--- src/pages/home/HomeScreen.tsx
+++ src/pages/home/HomeScreen.tsx
@@ -3,20 +3,18 @@ import { div_config } from "../../configs/FramerMotion";
 import { Banner } from "../../components/home/Banner";
 import { Skills } from "../../components/home/Skills";
 import { LetsTalkButton } from "../../components/generic/LetsTalkButton";
-import { ResumeButton } from "../../components/generic/ResumeButton";
 
 export const HomeScreen = () => {
     return (
         <motion.div {...div_config}>
             <div
                 id="Welcome"
-                className="min-h-screen flex items-center justify-center flex-col"
+                className="min-h-[80vh] flex items-center justify-center flex-col"
             >
                 <Banner />
 
                 <div className="mt-12">
                     <LetsTalkButton inverted />
-                    {/* <ResumeButton /> */}
                 </div>
             </div>
             <div
