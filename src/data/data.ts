a3096c887f32559a1d64fa567e997b6e7ccebfd8
Ivan Lara <uki@live.co.uk>
2022-06-21 09:33:12 -0600

    Fix about info

--- src/data/data.ts
+++ src/data/data.ts
@@ -98,6 +98,12 @@ export const skills: SkillsProps[] = [
     },
 ];
 
+export const about: string[] = [
+    "React/Angular frontend dev, .net/node js backend dev. I've been working just over 5 years ago with those techs, furthermore, I'm a Microsoft Professional certified in HTML5, CSS3, and JS.",
+    "Moreover, I have a massive passion for coding, the idea of contributing to user experience is priceless, and I love the way that anyone while coding, could be so creative. I believe this takes part of my own skillset. I started as a web designer, just making some visual coding and stylesheets, but since I knew about backend and frontend alternatives, I'm still growing my knowledge about development.",
+    "In conclusion, I have experience working on custom applications, e-commerce, micro websites, and Rest APIs. I'm open to grabbing another kind of knowledge with respect to other programming languages, I'm an active English student and kind of a lover of web/mobile development.",
+]
+
 export const projects: ProjectProps[] = [
     { id: "74283748374", name: "Blog with DatoCMS", link: 'https://blog-app-nine-omega.vercel.app/', github: 'https://github.com/larakalix/blog-app' },
     { id: "745757567vf", name: "Guess the Pokemon - Card Game", link: 'https://guess-the-pokemon-card.netlify.app/', github: 'https://github.com/larakalix/guess-cards-game' },
