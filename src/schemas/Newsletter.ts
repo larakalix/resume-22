a188a011dd257ca177dad34f4c323a2996329d92
Ivan Lara <ukikalix@gmail.com>
2022-08-24 01:57:57 -0600

    Add i18next to project and fix newsletter form validation messages

--- src/schemas/Newsletter.ts
+++ src/schemas/Newsletter.ts
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 
 export const SignupSchema = Yup.object().shape({
-    email: Yup.string().email("Invalid email").required("Required"),
+    email: Yup.string()
+        .email("Please enter a valid email")
+        .required("Please enter an email"),
 });
