9e812d868b754644e11a29754ca4dba18eb6aae4
Ivan Lara <ukikalix@gmail.com>
2023-03-23 01:06:54 -0600

    fx: ignore ui kit dist folder

--- packages/ui/dist/Button.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode;
-};
-declare const Button: React.FC<Props>;
-
-export { Button };
--- packages/ui/dist/index.mjs
+++
@@ -1,1 +0,0 @@
-export { a as Button } from "./chunk-5BG7LFX3.mjs";
